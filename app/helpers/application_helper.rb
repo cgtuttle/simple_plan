ced456c6b01913918b6e1938788a74207c345f20
Carl Tuttle <cgtuttle@gmail.com>
2011-04-02 11:04:52 -0500

    Verify views commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+# Return a title on a per-page basis.
+  def title
+    base_title = "Simple Plan"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+	
+	
+	def logo
+		logo = image_tag("logo.png", :alt => "Plan Well!", :class => "round")
+	end	
+	
 end
