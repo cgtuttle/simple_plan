86e3747eed3f73a9cd30bc835b9114edaf5de892
Carl Tuttle <cgtuttle@gmail.com>
2011-04-03 16:59:45 -0500

    Intirim commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 	
 	
 	def logo
-		logo = image_tag("logo.png", :alt => "Plan Well!", :class => "round")
+		logo = image_tag("SimplePlan.png", :alt => "Plan Well!", :class => "round")
 	end	
 	
 end
