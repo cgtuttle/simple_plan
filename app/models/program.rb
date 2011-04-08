4224af0e25028cc6e5c91f292facfd0353047566
Carl Tuttle <cgtuttle@gmail.com>
2011-04-08 10:06:31 -0500

    Corrected program.rb

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,6 +1,5 @@
 class Program < ActiveRecord::Base
-	attr_accessible :code, :name, :descr, :supplier_id, :category_id \
-		:rep_id, :customer_id, :start_date, :end_date
+	attr_accessible :code, :name, :descr, :supplier_id, :category_id, :rep_id, :customer_id, :start_date, :end_date
 
 	has_many :deals
 	belongs_to :customers
