1e6f7d70089aea3c0d65ebdaaa0a32f27fdb7910
Carl Tuttle <cgtuttle@gmail.com>
2011-04-24 10:51:49 -0500

    Added update, delete to activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -12,9 +12,23 @@ class ActivitiesController < ApplicationController
 	end
 
   def new
-		@activity = Activity.new
-		@title = "New Activity"
+		redirect_to activities_path
   end
+	
+	def edit
+		@activity = Activity.find(params[:id])
+		@title = @activity.name
+	end
+	
+	def update
+		@activity = Activity.find(params[:id])
+		if @activity.update_attributes(params[:activity])
+			flash[:success] = "Activity updated."
+			redirect_to activities_path
+		else
+			redirect_to activities_path
+		end
+	end
 	
 	def create
 		@activity = Activity.new(params[:activity])
@@ -26,5 +40,10 @@ class ActivitiesController < ApplicationController
 		end
 	end
 	
+	def destroy
+    Activity.find(params[:id]).destroy
+    flash[:success] = "Actvity deleted."
+    redirect_to activities_path
+  end	
 	
 end
