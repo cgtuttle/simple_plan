f81289421be49bab3e686bb9667d4d2805527e7e
Carl Tuttle <cgtuttle@gmail.com>
2011-05-03 15:47:34 -0500

    User model further setup

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,10 @@
 Factory.define :activity do |activity|
 	activity.name				"Test Activity"
 end
+
+Factory.define :user do |user|
+  user.name                  "Michael Hartl"
+  user.email                 "mhartl@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
