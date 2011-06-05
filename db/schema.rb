d2e4d5d42099bc4a17ceeb7d756b569011440e40
Carl Tuttle <cgtuttle@gmail.com>
2011-06-05 15:02:39 -0500

    Built Role and User_role models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110605194433) do
+ActiveRecord::Schema.define(:version => 20110605200120) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -83,6 +83,13 @@ ActiveRecord::Schema.define(:version => 20110605194433) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_roles", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
