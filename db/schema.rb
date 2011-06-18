56c16ae8e576c470c3967786378ad55a361fde62
Carl Tuttle <cgtuttle@gmail.com>
2011-06-18 09:35:53 -0500

    account_groups merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110612145113) do
+ActiveRecord::Schema.define(:version => 20110618142916) do
+
+  create_table "account_groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "accounts", :force => true do |t|
     t.string   "domain"
