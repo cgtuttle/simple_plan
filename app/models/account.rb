0b2ef6050f868cc4e895bc669fc03ef00fb563de
Carl Tuttle <cgtuttle@gmail.com>
2011-06-19 11:26:24 -0500

    Group changes complete

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ActiveRecord::Base
 
-	belongs_to :account_group, :foreign_key => "account_group"
+	belongs_to :group
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ActiveRecord::Base
+
+	has_many :accounts
+
 end
