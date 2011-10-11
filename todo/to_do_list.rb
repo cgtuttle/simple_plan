6265d4e4fe34a69f527c6f8048f6b1cf3ec4ace8
Carl Tuttle <cgtuttle@gmail.com>
2011-10-11 05:57:57 -0500

    10112011 update

---
+++ todo/to_do_list.rb
@@ -0,0 +1,74 @@
+To-do List
+----------------------------------------
+DONE:	change accounts table
+DONE:	change partnerships table
+DONE:		rename provider_id to supplier_id
+DONE:	change activities table
+DONE:		add code 15
+DONE:		name limit 127
+DONE:		add descr
+DONE:		add account_id
+DONE:	change products table
+DONE:		remove category_id
+DONE:		add account_id
+DONE:		change code: limit 15, null false
+DONE:		add name: limit 127
+DONE:	change profiles table
+DONE:		remove name
+DONE:	change users table
+DONE:		change role limit 63
+DONE:	add models
+DONE:		program_activities
+DONE:		program_customers
+DONE:		program_categories
+DONE:		category_products
+DONE:		program_products
+DONE:	update models w/belongs_to
+DONE:		program
+DONE:		activity
+DONE:		category
+DONE:		product
+DONE:	fix profiles show, edit view
+DONE:		no :name field available
+DONE:	change activities name
+DONE:		remove unique constraint
+			add controllers, resources
+DONE:		categories
+DONE:		programs
+				deals
+DONE:		products
+				category_products
+				program_categories
+				program_products
+				program_activities
+				program_customers
+			add views index, _new, show, edit
+DONE:		categories
+DONE:		products index, _new, edit, show
+DONE:		programs
+				deals
+				category_products
+				program_categories
+				program_products
+				program_activities
+				program_customers
+			add left menu items
+DONE:		categories
+				products
+DONE:		programs
+				deals
+			resize all code columns to 31
+DONE:	add Partners
+DONE:		model
+DONE:			account_id
+DONE:			partner_id
+DONE:			partner_type
+DONE:		controller/views
+DONE:	drop partnerships
+DONE:	rename partners to partnerships
+DONE:		model
+DONE:		controller
+DONE:		views
+			update 3.1 to assets
+			clean up css for tables
+			
